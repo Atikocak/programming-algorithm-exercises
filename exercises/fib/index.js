5121ce807c79f102370e3ef8ef5054de29b3adb0
atilimkocak <atilimkocak@gmail.com>
2024-06-09 21:34:53 +0300

    Fibonacci Algorithm (Recursive Optimization with Memoization)

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,50 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    };
+}
+
+function slowFib(n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
+
+/* function fib(n) { // Recursive solution (not efficient) O(2^n)
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+} */
+
+/* function fib(n) {    // Iterative solution (efficient) O(n)
+    if (n < 2) {
+        return n;
+    }
+
+    let result = [0, 1];
+    let i = 0;
+    while (result.length <= n) {
+        result.push(result[i] + result[i + 1]);
+        i++;
+    }
+
+    return result[n];
+} */
